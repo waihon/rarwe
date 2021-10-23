ee773f63bc3077e896ef052dc38a2c60a2a671fb
Waihon Yew <yewwaihon@gmail.com>
2021-10-23 14:58:52 +0800

    Implement a rough-cut version of star rating using ASCII

--- app/components/star-rating.js
+++ app/components/star-rating.js
@@ -1,3 +1,20 @@
 import Component from '@glimmer/component';
 
-export default class StarRatingComponent extends Component {}
+export default class StarRatingComponent extends Component {
+  get maxRating() {
+    return this.args.maxRating ?? 5;
+  }
+
+  // We need to define a stars property because in Ember templates,
+  // results of function calls cannot be rendered.
+  get stars() {
+    let stars = [];
+    for (let i = 1; i <= this.maxRating; i++) {
+      stars.push({
+        rating: i,
+        full: i <= this.args.rating,
+      });
+    }
+    return stars;
+  }
+}
