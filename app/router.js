4a328863e0cda4247826196c04f02b1d65497a21
Waihon Yew <yewwaihon@gmail.com>
2021-10-23 08:51:32 +0800

    Lint and fix source files

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function () {
-  this.route('bands', function() {
+  this.route('bands', function () {
     this.route('band', { path: ':id' }, function () {
       this.route('songs');
     });
--- tests/unit/routes/bands/band-test.js
+++ tests/unit/routes/bands/band-test.js
@@ -1,10 +1,10 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | bands/band', function(hooks) {
+module('Unit | Route | bands/band', function (hooks) {
   setupTest(hooks);
 
-  test('it exists', function(assert) {
+  test('it exists', function (assert) {
     let route = this.owner.lookup('route:bands/band');
     assert.ok(route);
   });
