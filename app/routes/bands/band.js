a86a2e5530fcc745ab7a2f67406998bf77e13ec0
Waihon Yew <yewwaihon@gmail.com>
2021-10-23 08:20:10 +0800

    Update bands.band route to find the band represented by the :id path

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default class BandsBandRoute extends Route {
+  model(params) {
+    // modelFor fetches the model of a parent route that had already
+    // been activated.
+    let bands = this.modelFor('bands');
+    return bands.find((band) => band.id == params.id);
+  }
 }
