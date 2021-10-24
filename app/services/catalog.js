1f7f1bf8fe362ef1ee7b2f94e5bd2f942ec43c09
Waihon Yew <yewwaihon@gmail.com>
2021-10-24 23:04:53 +0800

    Implement a catalog service

--- app/services/catalog.js
+++ app/services/catalog.js
@@ -1,3 +1,25 @@
 import Service from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
-export default class CatalogService extends Service {}
+export default class CatalogService extends Service {
+  @tracked storage = {};
+
+  constructor() {
+    super(...arguments);
+    this.storage.bands = [];
+    this.storage.songs = [];
+  }
+
+  add(type, record) {
+    let collection = type == 'band' ? this.storage.bands : this.storage.songs;
+    collection.push(record);
+  }
+
+  get bands() {
+    return this.storage.bands;
+  }
+
+  get songs() {
+    return this.storage.songs;
+  }
+}
