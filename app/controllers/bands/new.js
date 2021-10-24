c00abb63838ce872eba500bb73df0a811eeda76d
Waihon Yew <yewwaihon@gmail.com>
2021-10-24 23:12:09 +0800

    Add a new band to the catalog service

--- app/controllers/bands/new.js
+++ app/controllers/bands/new.js
@@ -3,8 +3,11 @@ import { action } from '@ember/object';
 import { dasherize } from '@ember/string';
 import { tracked } from '@glimmer/tracking';
 import { Band } from 'rarwe/routes/bands';
+import { inject as service } from '@ember/service';
 
 export default class BandsNewController extends Controller {
+  @service catalog;
+
   @tracked name;
 
   @action
@@ -14,6 +17,7 @@ export default class BandsNewController extends Controller {
 
   @action
   saveBand() {
-    new Band({ name: this.name, id: dasherize(this.name) });
+    let band = new Band({ name: this.name, id: dasherize(this.name) });
+    this.catalog.add('band', band);
   }
 }
