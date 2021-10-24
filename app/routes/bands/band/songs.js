d5e3674bbeb1ad93abeea7070cd86b4beb137959
Waihon Yew <yewwaihon@gmail.com>
2021-10-24 17:03:48 +0800

    Set band property to be accessed by the controller

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -5,4 +5,12 @@ export default class BandsBandSongsRoute extends Route {
     let band = this.modelFor('bands.band');
     return band.songs;
   }
+
+  // Ember's routing layer provides a hook that comes handy when you want to set properties
+  // on the controller: setupController. As we can access the band in the route, we can
+  // leverage the hook to set it.
+  setupController(controller) {
+    super.setupController(...arguments);
+    controller.set('band', this.modelFor('bands.band'));
+  }
 }
