21237745c3c2509ee94fe01155f311bc99a3b561
Waihon Yew <yewwaihon@gmail.com>
2021-10-24 17:15:06 +0800

    Rely on the default implementation of the model hook to return the model of the parent route, band in this case

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,16 +1,6 @@
 import Route from '@ember/routing/route';
 
 export default class BandsBandSongsRoute extends Route {
-  model() {
-    let band = this.modelFor('bands.band');
-    return band.songs;
-  }
-
-  // Ember's routing layer provides a hook that comes handy when you want to set properties
-  // on the controller: setupController. As we can access the band in the route, we can
-  // leverage the hook to set it.
-  setupController(controller) {
-    super.setupController(...arguments);
-    controller.set('band', this.modelFor('bands.band'));
-  }
+  // The default implementation of the model hook is to return the model of the
+  // parent route. Since we need the band as the model, that suits us perfectly.
 }
