0a05a896d887649a8688dc64808ebb80a0f0ec86
Waihon Yew <yewwaihon@gmail.com>
2021-10-25 18:01:20 +0800

    Extract band and song model classes

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,25 +1,7 @@
 import Route from '@ember/routing/route';
-import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
-
-export class Band {
-  @tracked name;
-  @tracked songs;
-
-  constructor({ id, name, songs }) {
-    this.id = id;
-    this.name = name;
-    this.songs = songs || [];
-  }
-}
-
-export class Song {
-  constructor({ title, rating, band }) {
-    this.title = title;
-    this.rating = rating ?? 0;
-    this.band = band;
-  }
-}
+import Band from 'rarwe/models/band';
+import Song from 'rarwe/models/song';
 
 export default class BandsRoute extends Route {
   @service catalog;
