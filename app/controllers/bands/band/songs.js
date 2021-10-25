19f1488445a56cdb1aa5ce6f1cec59cbf7a8eb3e
Waihon Yew <yewwaihon@gmail.com>
2021-10-25 23:55:52 +0800

    Update song ratings by sending a PATCH request to API server

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -3,6 +3,7 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import Song from 'rarwe/models/song';
 import { inject as service } from '@ember/service';
+import fetch from 'fetch';
 
 export default class BandsBandSongsController extends Controller {
   @tracked showAddSong = true;
@@ -16,10 +17,65 @@ export default class BandsBandSongsController extends Controller {
   }
 
   @action
-  saveSong() {
-    // this.model refers the the parent route's model by default, i.e. band
-    let song = new Song({ title: this.title, band: this.model });
+  async updateRating(song, rating) {
+    song.rating = rating;
+    let payload = {
+      data: {
+        id: song.id,
+        type: 'songs',
+        // As this is a PATCH request, we only need to send the changes: the new rating
+        attributes: {
+          rating
+        }
+      }
+    };
+    await fetch(`/songs/${song.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/vnd.api+json'
+      },
+      body: JSON.stringify(payload)
+    });
+  }
+
+  @action
+  async saveSong() {
+    let payload = {
+      data: {
+        type: 'songs',
+        attributes: {
+          title: this.title
+        },
+        relationships: {
+          band: {
+            data: {
+              // The designation of a related record by specifying its id and type
+              // is called "resource linkage" in the JSON:API vocabulary.
+              // this.model refers to the parent route's model by default, i.e. band
+              id: this.model.id,
+              type: 'bands'
+            },
+          },
+        },
+      },
+    };
+    let response = await fetch('/songs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/vnd.api+json'
+      },
+      body: JSON.stringify(payload)
+    });
+
+    let json = await response.json();
+    let { id, attributes, relationships } = json.data;
+    let rels = [];
+    for (let relationshipName in relationships) {
+      rels[relationshipName] = relationships[relationshipName].links.related;
+    }
+    let song = new Song({ id, ...attributes }, rels);
     this.catalog.add('song', song);
+
     this.model.songs = [...this.model.songs, song];
     this.title = '';
     this.showAddSong = true;
