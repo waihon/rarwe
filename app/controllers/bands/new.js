882f9a90b04ff111a269c6d0b064124e90207cae
Waihon Yew <yewwaihon@gmail.com>
2021-10-28 23:05:12 +0800

    Switch over the saving of a new band to use the create method on the catalog

--- app/controllers/bands/new.js
+++ app/controllers/bands/new.js
@@ -1,9 +1,7 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-import Band from 'rarwe/models/band';
 import { inject as service } from '@ember/service';
-import fetch from 'fetch';
 
 export default class BandsNewController extends Controller {
   @service catalog;
@@ -18,32 +16,7 @@ export default class BandsNewController extends Controller {
 
   @action
   async saveBand() {
-    let response = await fetch('/bands', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/vnd.api+json',
-      },
-      // The payload must be a JSON:API representation of the resource to be
-      // created: data with type and attributes.
-      body: JSON.stringify({
-        data: {
-          type: 'bands',
-          attributes: {
-            name: this.name,
-          },
-        },
-      }),
-    });
-    let json = await response.json();
-    // Creating the id of the band has now become the responsibility of the
-    // backend: the response contains a top-level id and the attributes.
-    let { id, attributes, relationships } = json.data;
-    let rels = [];
-    for (let relationshipName in relationships) {
-      rels[relationshipName] = relationships[relationshipName].links.related;
-    }
-    let record = new Band({ id, ...attributes }, rels);
-    this.catalog.add('band', record);
-    this.router.transitionTo('bands.band.songs', id);
+    let band = await this.catalog.create('band', { name: this.name });
+    this.router.transitionTo('bands.band.songs', band.id);
   }
 }
