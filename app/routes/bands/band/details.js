3dbb82a57a29c5d5eada89a21ccfe52330cff8e6
Waihon Yew <yewwaihon@gmail.com>
2021-10-29 17:53:40 +0800

    Redirect to details route if the song's description exists, songs route otherwise

--- app/routes/bands/band/details.js
+++ app/routes/bands/band/details.js
@@ -1,3 +1,9 @@
 import Route from '@ember/routing/route';
 
-export default class BandsBandDetailsRoute extends Route {}
+export default class BandsBandDetailsRoute extends Route {
+  // Child routes inherit the model of their parent by default.
+  // Otherwise, we'll have to write below code for example.
+  // model() {
+  //   return this.modelFor('bands.band');
+  // }
+}
