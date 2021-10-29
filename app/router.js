9e7f979baabc60f91435de2abfd58037f196d6a9
Waihon Yew <yewwaihon@gmail.com>
2021-10-29 17:05:02 +0800

    Generate a new route called bands.band.details

--- app/router.js
+++ app/router.js
@@ -10,6 +10,7 @@ Router.map(function () {
   this.route('bands', { path: '/' }, function () {
     this.route('band', { path: 'bands/:id' }, function () {
       this.route('songs');
+      this.route('details');
     });
     this.route('new', { path: 'bands/new' });
   });
