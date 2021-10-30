5c28885a1b83ce5091d7422b196a536685965be3
Waihon Yew <yewwaihon@gmail.com>
2021-10-30 21:22:57 +0800

    Test catalog service is able to store and retrieve bands as well as songs

--- tests/unit/services/catalog-test.js
+++ tests/unit/services/catalog-test.js
@@ -1,12 +1,36 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import Band from 'rarwe/models/band';
+import Song from 'rarwe/models/song';
 
 module('Unit | Service | catalog', function (hooks) {
   setupTest(hooks);
 
-  // TODO: Replace this with your real tests.
-  test('it exists', function (assert) {
-    let service = this.owner.lookup('service:catalog');
-    assert.ok(service);
+  test('it can store and retrieve bands', function (assert) {
+    // this.owner gives access to the application instance running in
+    // the test container.
+    // Its lookup method retrieves the referred entity from the registry.
+    // service:catalog, controller:bands/new, and component:star-rating
+    // are valid values, the first part denoting the "type" of the entity
+    // we want to fetch.
+    let catalog = this.owner.lookup('service:catalog');
+    catalog.add('band', new Band({ id: 1, name: 'Led Zeppelin' }));
+
+    assert.equal(catalog.bands.length, 1);
+    assert.equal(catalog.bands[0].name, 'Led Zeppelin');
+  });
+
+  test('it can store and retrieve songs', function (assert) {
+    let catalog = this.owner.lookup('service:catalog');
+    catalog.add(
+      'song',
+      new Song({
+        id: 1,
+        title: 'Achilles Last Stand',
+        rating: 5,
+      })
+    );
+    assert.equal(catalog.songs.length, 1);
+    assert.equal(catalog.songs[0].title, 'Achilles Last Stand');
   });
 });
