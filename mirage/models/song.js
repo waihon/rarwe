2d29ff73b5394e845a7f4a0fec5b624491a53708
Waihon Yew <yewwaihon@gmail.com>
2021-10-30 12:53:08 +0800

    Update band and song Mirage models with reciprocal relationships

--- mirage/models/song.js
+++ mirage/models/song.js
@@ -1,3 +1,11 @@
-import { Model } from 'ember-cli-mirage';
+import { Model, belongsTo } from 'ember-cli-mirage';
 
-export default Model.extend({});
+export default Model.extend({
+  // If the project has Ember Data models, Mirage auto detects them and
+  // manually creating Mirage model is not necessary.
+  //
+  // Simple properties don't need to be defined, but defining relationships
+  // gives us a few methods on the relationship property we can use to set up
+  // our data hierarchy.
+  band: belongsTo(),
+});
