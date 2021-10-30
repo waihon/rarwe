ba5620eb4181221db4df1e7e22ebc9ba9c48d0ea
Waihon Yew <yewwaihon@gmail.com>
2021-10-30 21:23:55 +0800

    Test catalog service is able to load a record from a JSON:API response

--- tests/unit/services/catalog-test.js
+++ tests/unit/services/catalog-test.js
@@ -33,4 +33,27 @@ module('Unit | Service | catalog', function (hooks) {
     assert.equal(catalog.songs.length, 1);
     assert.equal(catalog.songs[0].title, 'Achilles Last Stand');
   });
+
+  test('it can load a record from a JSON:API response', function (assert) {
+    let catalog = this.owner.lookup('service:catalog');
+    catalog.load({
+      data: {
+        type: 'bands',
+        id: '1',
+        attributes: {
+          name: 'TOOL',
+        },
+        relationships: {
+          songs: {
+            links: {
+              related: '/bands/1/songs',
+            },
+          },
+        },
+      },
+    });
+    let band = catalog.bands[0];
+    assert.equal(band.name, 'TOOL');
+    assert.equal(band.relationships.songs, '/bands/1/songs');
+  });
 });
