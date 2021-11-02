0899a360ad6e03c718b7acf79b789e67edea1525
Waihon Yew <yewwaihon@gmail.com>
2021-11-03 00:05:07 +0800

    Restore the default response to the loading event being triggered

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,14 +1,8 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
 
 export default class BandsRoute extends Route {
   @service catalog;
-
-  @action
-  loading() {
-    window.alert("Loading the bands and/or songs, ok?");
-  }
 
   async model() {
     return this.catalog.fetchAll('bands');
